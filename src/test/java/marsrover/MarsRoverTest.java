c534e1f3d926ae63986ededeb0bce8ea4f9744a4
Niek Vervaeke <niek.vervaeke@gmail.com>
2019-02-26 13:12:12 +0100

    keep expected close to assert

--- src/test/java/marsrover/MarsRoverTest.java
+++ src/test/java/marsrover/MarsRoverTest.java
@@ -13,89 +13,89 @@ public class MarsRoverTest {
 
     @Test
     public void defaultPosition() {
-        final Position expected = new Position(0, 0, NORTH);
         final MarsRover marsRover = new MarsRover();
 
+        final Position expected = new Position(0, 0, NORTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void specificInitialPosition() {
-        final Position expected = new Position(1, 1, SOUTH);
         final Position landingCoordinate = new Position(1, 1, SOUTH);
         final MarsRover marsRover = new MarsRover(landingCoordinate);
 
+        final Position expected = new Position(1, 1, SOUTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void forward() {
-        final Position expected = new Position(0, 1, NORTH);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("F");
 
+        final Position expected = new Position(0, 1, NORTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void towStepsForward() {
-        final Position expected = new Position(0, 2, NORTH);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("F", "F");
 
+        final Position expected = new Position(0, 2, NORTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void backward() {
-        final Position expected = new Position(0, -1, NORTH);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("B");
 
+        final Position expected = new Position(0, -1, NORTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void twoStepsBackward() {
-        final Position expected = new Position(0, -2, NORTH);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("B", "B");
 
+        final Position expected = new Position(0, -2, NORTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void left() {
-        final Position expected = new Position(0, 0, WEST);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("L");
 
+        final Position expected = new Position(0, 0, WEST);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     public void right() {
-        final Position expected = new Position(0, 0, EAST);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("R");
 
+        final Position expected = new Position(0, 0, EAST);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 
     @Test
     @Disabled // TODO
     public void left_left() {
-        final Position expected = new Position(0, 0, SOUTH);
         final MarsRover marsRover = new MarsRover();
 
         marsRover.execute("L", "L");
 
+        final Position expected = new Position(0, 0, SOUTH);
         assertThat(marsRover.currentPosition()).isEqualTo(expected);
     }
 }
